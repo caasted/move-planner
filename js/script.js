3bee6de7f3910bc89a54d13d57770eeae9b45edf
Chris Aasted <caasted@gmail.com>
2016-10-31 14:38:16 -0700

    feat: Add wikipedia API request and display links

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ function loadData() {
 
     // load nytimes
     // obviously, replace all the "X"s with your own API key
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=24075f2187a74e17a942c059b8b6ba11';
     $.getJSON(nytimesUrl, function(data){
 
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
@@ -50,6 +50,29 @@ function loadData() {
     // load wikipedia data
 
     // YOUR CODE GOES HERE!
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+
+    var wikiRequestTimeout = setTimeout(function(){
+        $wikiElem.text("Failed to get wikipedia resources");
+    }, 8000);
+
+    $.ajax({
+        url: wikiUrl, 
+        dataType: "jsonp", 
+        // jsonp: "callback", 
+    }).done(function( response ) {
+        var articleList = response[1];
+
+        for (var i = 0; i < articleList.length; i++) {
+            articleStr = articleList[i];
+            var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+            $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+        }
+
+        clearTimeout(wikiRequestTimeout);
+    })
+
+
 
     return false;
 };
